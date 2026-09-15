c83629e0396e4794936b263c6af6320333e335f1
Luxas <135403604+Spl0itable@users.noreply.github.com>
2026-09-15 17:43:27 +0930

    v1.0.29

--- tools/gen-index.mjs
+++ tools/gen-index.mjs
@@ -142,13 +142,13 @@ ${ART}
                             <span class="store-badge-store">App Store</span>
                         </span>
                     </span>
-                    <span class="store-badge is-pending" title="Coming soon">
+                    <a href="https://play.google.com/store/apps/details?id=ai.nymbot" class="store-badge" target="_blank" rel="noopener">
                         <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M22.018 13.298l-3.919 2.218-3.515-3.493 3.543-3.521 3.891 2.202a1.49 1.49 0 0 1 0 2.594zM1.337.924a1.486 1.486 0 0 0-.112.568v21.017c0 .217.045.419.124.6l11.155-11.087L1.337.924zm12.207 10.065l3.258-3.238L3.45.195a1.466 1.466 0 0 0-.946-.179l11.04 10.973zm0 2.067l-11 10.933c.298.036.612-.016.906-.183l13.324-7.54-3.23-3.21z"/></svg>
                         <span class="store-badge-text">
-                            <span class="store-badge-label">Coming soon to</span>
+                            <span class="store-badge-label">Get it on</span>
                             <span class="store-badge-store">Google Play</span>
                         </span>
-                    </span>
+                    </a>
                     <a href="https://zapstore.dev/apps/ai.nymbot" class="store-badge" target="_blank" rel="noopener">
                         <svg viewBox="0 0 19 32" xmlns="http://www.w3.org/2000/svg"><path d="M18.8379 13.9711L8.84956 0.356086C8.30464 -0.386684 7.10438 0.128479 7.30103 1.02073L9.04686 8.94232C9.16268 9.46783 8.74887 9.96266 8.19641 9.9593L0.871032 9.91477C0.194934 9.91066 -0.223975 10.6293 0.126748 11.1916L7.69743 23.3297C7.99957 23.8141 7.73264 24.4447 7.16744 24.5816L5.40958 25.0076C4.70199 25.179 4.51727 26.0734 5.10186 26.4974L12.4572 31.8326C12.9554 32.194 13.6711 31.9411 13.8147 31.3529L15.8505 23.0152C16.0137 22.3465 15.3281 21.7801 14.6762 22.0452L13.0661 22.7001C12.5619 22.9052 11.991 22.6092 11.8849 22.0877L10.7521 16.5224C10.6486 16.014 11.038 15.5365 11.5704 15.5188L18.1639 15.2998C18.8529 15.2769 19.2383 14.517 18.8379 13.9711Z"/></svg>
                         <span class="store-badge-text">
@@ -315,13 +315,13 @@ ${FAQ}
                         <span class="store-badge-store">App Store</span>
                     </span>
                 </span>
-                <span class="store-badge is-pending" title="Coming soon">
+                <a href="https://play.google.com/store/apps/details?id=ai.nymbot" class="store-badge" target="_blank" rel="noopener">
                     <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M22.018 13.298l-3.919 2.218-3.515-3.493 3.543-3.521 3.891 2.202a1.49 1.49 0 0 1 0 2.594zM1.337.924a1.486 1.486 0 0 0-.112.568v21.017c0 .217.045.419.124.6l11.155-11.087L1.337.924zm12.207 10.065l3.258-3.238L3.45.195a1.466 1.466 0 0 0-.946-.179l11.04 10.973zm0 2.067l-11 10.933c.298.036.612-.016.906-.183l13.324-7.54-3.23-3.21z"/></svg>
                     <span class="store-badge-text">
-                        <span class="store-badge-label">Coming soon to</span>
+                        <span class="store-badge-label">Get it on</span>
                         <span class="store-badge-store">Google Play</span>
                     </span>
-                </span>
+                </a>
                 <a href="https://zapstore.dev/apps/ai.nymbot" class="store-badge" target="_blank" rel="noopener">
                     <svg viewBox="0 0 19 32" xmlns="http://www.w3.org/2000/svg"><path d="M18.8379 13.9711L8.84956 0.356086C8.30464 -0.386684 7.10438 0.128479 7.30103 1.02073L9.04686 8.94232C9.16268 9.46783 8.74887 9.96266 8.19641 9.9593L0.871032 9.91477C0.194934 9.91066 -0.223975 10.6293 0.126748 11.1916L7.69743 23.3297C7.99957 23.8141 7.73264 24.4447 7.16744 24.5816L5.40958 25.0076C4.70199 25.179 4.51727 26.0734 5.10186 26.4974L12.4572 31.8326C12.9554 32.194 13.6711 31.9411 13.8147 31.3529L15.8505 23.0152C16.0137 22.3465 15.3281 21.7801 14.6762 22.0452L13.0661 22.7001C12.5619 22.9052 11.991 22.6092 11.8849 22.0877L10.7521 16.5224C10.6486 16.014 11.038 15.5365 11.5704 15.5188L18.1639 15.2998C18.8529 15.2769 19.2383 14.517 18.8379 13.9711Z"/></svg>
                     <span class="store-badge-text">
